31ed53aa9c58c249491c8cdc9cfd8a73fbb4701a
Ryan Pessa <ryan@essential-elements.net>
2016-02-18 14:52:32 -0600

    add password dialog to settings

---
+++ src/com/android/settings/SettingsPassword.java
@@ -0,0 +1,52 @@
+package com.android.settings;
+
+import java.lang.System;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class SettingsPassword extends Activity {
+    private EditText txtPassword;
+    private Button btnSubmit;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_password);
+        addListenerOnButton();
+    }
+
+    private static String getPassword() {
+        String pw = System.getProperty("aeris.settings.password");
+        if (pw == null) {
+            return "1234567890";
+        }
+        return pw;
+    }
+
+    private void addListenerOnButton() {
+        txtPassword = (EditText)findViewById(R.id.txtPassword);
+        btnSubmit = (Button)findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String pw = txtPassword.getText().toString();
+                if (!pw.equals(getPassword())) {
+                    Toast.makeText(SettingsPassword.this, "Access Denied", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Intent settings = new Intent(SettingsPassword.this, Settings.class);
+                startActivity(settings);
+            }
+        });
+    }
+}
